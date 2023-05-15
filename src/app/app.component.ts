a8b3fd441d0aec2e0c48194e74905dcd184be38f
paulguyet <pgt4711@gmail.com>
2023-05-15 13:41:36 +0200

    NEW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taboo-game';
+  infoText = 'Your guesses as well as your score and the values of the timer at different points during the game are stored anonymously in a database for scientific purposes.';
+
+  gameStarted = false;
+
+  onStartGame() {
+    this.gameStarted = true;
+  }
+
+  onTimeOver() {
+    this.gameStarted = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GuessInputComponent } from './components/guess-input/guess-input.component';
+import { WordDescriptionComponent } from './components/word-description/word-description.component';
+import { GameDisplayComponent } from './components/game-display/game-display.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuessInputComponent,
+    WordDescriptionComponent,
+    GameDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
