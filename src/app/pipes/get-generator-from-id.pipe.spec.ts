fd7352e7432a685a76c69d2732d00e90243c9dc7
paulguyet <pgt4711@gmail.com>
2023-05-23 17:38:51 +0200

    Paar Statistic-Cards hinzugefuegt und den Generator der Description

---
+++ src/app/pipes/get-generator-from-id.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetGeneratorFromIdPipe } from './get-generator-from-id.pipe';
+
+describe('GetGeneratorFromIdPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetGeneratorFromIdPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
